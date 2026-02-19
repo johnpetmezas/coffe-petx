953d0b9e0fe04bef96800f2588c7656e17c078cf
johnpetmezas <163676421+johnpetmezas@users.noreply.github.com>
2026-02-20 01:27:58 +0200

    Upload tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "#050505",
+                foreground: "#ECE0D1",
+                coffee: {
+                    black: "#050505",
+                    bone: "#ECE0D1",
+                    grey: "#A8A29E",
+                    gold: "#D4A373",
+                }
+            },
+            fontFamily: {
+                sans: ['var(--font-inter)', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
+export default config;
